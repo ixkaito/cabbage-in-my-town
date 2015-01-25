5f4855d068c564f504430972613b2936623e1fe8
satomicchy <satomi_t_sora@yahoo.co.jp>
2015-01-25 16:49:20 +0900

    scaffold Cost

---
+++ app/models/cost.rb
@@ -0,0 +1,5 @@
+class Cost < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+  belongs_to :shop
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :costs
+
   resources :products
 
   resources :shops
