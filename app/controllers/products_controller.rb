d11ddd15c6fcadae2998b0570bc797cd8e3303c6
KITE <ixkaito@gmail.com>
2015-01-31 13:50:20 +0900

    Enable creating products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,7 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
+    @categories = Category.all
   end
 
   # GET /products/1/edit
@@ -69,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, :category_id)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   belongs_to :category, foreign_key: :category_id
+  validates :name, presence: true
+  validates :category_id, presence: true
 end
